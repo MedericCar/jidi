abdb025f82a3b0bdfcd2a03187e28e64cd7be192
Mederic Carriat <mederic.carriat@salesforce.com>
2024-05-26 09:40:50 +0200

    Add typing effect

--- frontend/src/components/QuestionComponent.js
+++ frontend/src/components/QuestionComponent.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './QuestionComponent.css';
 
-const QuestionComponent = ({ question, choices, selectedChoices, onSelectChoice }) => (
-  <div className="question-container">
-    <h2 className="question">{question}</h2>
-    <div className="choices-container">
-      {choices.map((choice, index) => (
-        <div
-          key={index}
-          className={`choice-box ${selectedChoices.includes(choice) ? 'selected' : ''}`}
-          onClick={() => onSelectChoice(choice)}
-        >
-          {choice}
-        </div>
-      ))}
+const QuestionComponent = ({ question, choices, selectedChoices, onSelectChoice }) => {
+  const [displayedQuestion, setDisplayedQuestion] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // Reset the state when the question prop changes
+    setDisplayedQuestion('');
+    setCurrentIndex(0);
+  }, [question]);
+
+  useEffect(() => {
+    if (currentIndex < question.length) {
+      const timeout = setTimeout(() => {
+        setDisplayedQuestion(prevText => prevText + question[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, 40);
+  
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, question]);
+
+  return (
+    <div className="question-container">
+      <h2 className="question">{displayedQuestion}</h2>
+      <div className="choices-container">
+        {choices.map((choice, index) => (
+          <div
+            key={index}
+            className={`choice-box ${selectedChoices.includes(choice) ? 'selected' : ''}`}
+            onClick={() => onSelectChoice(choice)}
+          >
+            {choice}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default QuestionComponent;
